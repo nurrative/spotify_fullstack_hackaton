8f0553ab34cb756b44992da52958245b4a035e75
nurrative <nursul1997@gmail.com>
2023-06-02 13:14:49 +0600

    change settings

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,7 @@ SWAGGER_SETTINGS = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
 }
 
